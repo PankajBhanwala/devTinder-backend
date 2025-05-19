c6ca958cc96f7144dbeeb4080de4f537c215aada
Hello-Pankaj <pankajbhanwalasnp@gmail.com>
2025-05-20 02:03:06 +0530

    correction in package.json

--- src/app.js
+++ src/app.js
@@ -2,14 +2,18 @@ const express = require('express');
 
 const app = express();
 
+// will work
+app.use("/namaste", (req, res) => {
+    res.send("namaste ji namaste");
+});
+
 app.use("/", (req, res) => {
     res.send("Hello from server");
 });
 
-
 // will not work
-app.use("/namaste", (req, res) => {
-    res.send("namaste ji namaste");
+app.use("/test", (req, res) => {
+    res.send("test server");
 });
 
 app.listen(3000, () => {
